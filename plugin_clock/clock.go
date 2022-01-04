ba3b503797e5b4dc275f47be127bbb72320e51aa
winefurce <waterwx17@gmail.com>
2022-01-04 13:33:40 +0800

    clock change

--- plugin_clock/clock.go
+++ plugin_clock/clock.go
@@ -1,24 +1,20 @@
 package clock
 
 import (
+	"encoding/base64"
 	"fmt"
 	"github.com/FloatTech/ZeroBot-Plugin/control"
 	"github.com/fumiama/cron"
 	zero "github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/message"
-	"math/rand"
+	"io/ioutil"
 	"time"
 )
-
-const (
-	res = "https://github.com/fallnight/ZeroBot-Plugin/tree/master/plugin_clock/hibiki/"
-)
-
 
 func init() { // 插件主体
 	// 定时任务每天10点执行一次0 0 8-0 * *
 	c := cron.New()
-	_, err := c.AddFunc("25 21 * * *", func() { sendNotice() })
+	_, err := c.AddFunc("0 * * * *", func() { sendNotice() })
 	if err == nil {
 		c.Start()
 	}
@@ -67,16 +63,14 @@ func sendNotice() {
 			for _, g := range ctx.GetGroupList().Array() {
 				grp := g.Get("group_id").Int()
 				if m.IsEnabledIn(grp) {
-					var hour int = time.Now().Hour()
-					var hourStr string = fmt.Sprintf("%d", hour)
-					ctx.SendGroupMessage(grp,randRecord( hourStr + ".mp3"))
+					hour := time.Now().Hour()
+					hourStr := fmt.Sprintf("%02d", hour)
+					f,_ := ioutil.ReadFile("D:/ZeroBot-Plugin/plugin_clock/hibiki/" + hourStr + ".wav")
+					bufSotre := base64.StdEncoding.EncodeToString(f)
+					ctx.SendGroupMessage(grp,message.Record(fmt.Sprintf("base64://%v",bufSotre)))
 				}
 			}
 			return true
 		})
 	}
-}
-
-func randRecord(file ...string) message.MessageSegment {
-	return message.Record(res + file[rand.Intn(len(file))])
 }
